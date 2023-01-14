d68026f64c69bb6b36fb2d4d92656950e246a290
maths0607 <mathiaskoba0607@gmail.com>
2023-01-14 23:28:14 +0000

    petie correctin en 15 (je pense)correction

--- code.c
+++ code.c
@@ -4,7 +4,7 @@
 #include <math.h>
  
  
-void saisi (float a,float b , float c  );
+void saisi (float a,float b , float c ,float *x,float *y,float*z );
 int validite(float a,float b, float c );
 float perimetre (float a,float b, float c);
 float air(float a,float b, float c);
@@ -12,33 +12,32 @@ float air(float a,float b, float c);
  
 int main()
 {   float a,b,c,x,y,z,perimetr,surface;
-    bool resultat;
+    int resultat;
     
      
     do{
      printf("Saisir les entier a , b et c \n");
 
-     saisi(a,b,c);
+     saisi(a,b,c,&x,&y,&z);
 
      resultat = validite(x,y,z);
 
      if(resultat==0){ printf("erreur \n");}
 
     }while(resultat==0);
-    printf("Le perimetre est %f ", perimetre(a,b,c));
-    printf("Le surface est %f ",   air(a,b,c));
+    printf("Le perimetre est %f ", perimetre(x,y,z));
+    printf("Le surface est %f ",   air(x,y,z));
     
     return 0;
 }
-void saisi (float a,float b , float c  )
+void saisi (float a,float b , float c,float *x,float *y,float *z ) 
 {    printf("\n");
     scanf("%f\n",&a);
     scanf("%f\n",&b);
     scanf("%f",&c);
     *x=a;
     *y=b;
-    *z=c
-    
+    *z=c;
    
 }
 
